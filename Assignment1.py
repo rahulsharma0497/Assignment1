c85839e38f1e4a05da211233df6d4678dba808e2
rahulsharma0497 <115954726+rahulsharma0497@users.noreply.github.com>
2023-07-11 23:48:54 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,19 @@
+def twoSum(nums, target):
+    num_indices = {}
+
+    for i, num in enumerate(nums):
+        complement = target - num
+
+        if complement in num_indices:
+            return [num_indices[complement], i]
+
+        num_indices[num] = i
+
+    return []
+
+
+# Example usage
+nums = [2, 7, 11, 15]
+target = 9
+result = twoSum(nums, target)
+print(result)
